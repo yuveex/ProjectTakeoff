d027608794c1dc4f5792873d4fc8432a4bbc2a51
yuveex <alvinsetiawan2003@gmail.com>
2024-04-09 15:01:20 +0700

    Models created

---
+++ ProjectTakeoff/app/src/main/java/com/ubaya/projecttakeoff/model/Article.kt
@@ -0,0 +1,10 @@
+package com.ubaya.projecttakeoff.model
+
+data class Article(
+    var id: String,
+    var title: String,
+    var content: String,
+    var summary: String,
+    var image_url: String,
+    var author_id: String,
+)
