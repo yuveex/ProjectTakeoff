cc6e86a3fca9623de95c996b71ba7427d60444ce
yuveex <alvinsetiawan2003@gmail.com>
2024-04-09 15:09:37 +0700

    Insert Material Design Theme

---
+++ ProjectTakeoff/app/src/main/java/com/ubaya/projecttakeoff/view/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.ubaya.projecttakeoff.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ubaya.projecttakeoff.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
